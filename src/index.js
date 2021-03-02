af681883c4b649e9a58445ec2ed4daf60c44e639
Dmytro Motuzko <motuzko.d15@gmail.com>
2021-03-02 21:35:19 +0200

    init: work with arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    } else {
+        return (Math.min.apply(null, array));
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    } else {
+        return (Math.max.apply(null, array));
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    } else {
+        return (array.reduce(function(a, b) {
+            return a + b;
+        })) / array.length;
+    }
 }
